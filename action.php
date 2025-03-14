05236ee6bdcdd9875cbf31307a66175d02db2b7d
Anannya <124086869+Nouroze-Tarannum-Anannya@users.noreply.github.com>
2025-03-15 01:01:41 +0600

    first commit

---
+++ action.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+</head>
+<body>
+    
+      <h2>Registration successful</h2>
+
+</body>
+</html>
---
+++ registration_form_admin.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Seller Registration Form</title>
+</head>
+
+<body>
+    <h2>Seller Registration Form</h2>
+
+    <fieldset>
+    <legend> </legend>
+    
+    <form action="action.php" method="post" target="_blank">
+
+    <table>
+        <tr>
+             <td><img src ="../images/icname.png" width="18px"> First Name:</td>
+             <td>  <input type="text" name="firstname" require></td>
+             <td> Last Name: <input type="text" name="lastname" require></td>
+
+</tr>
+
+    <tr>
+        <td><img src ="../images/icphn.png" width="18px"> Phone Number:</td>
+        <td> <input type="text" name="phonenumber" required></td>
+
+</tr>
+
+    <tr>
+         <td><img src ="../images/icgmail.png" width="18px"> G-mail:</td>
+         <td><input type="email" name="email"  required></td>
+
+</tr>
+   
+    <tr>
+
+         <td><img src ="../images/icpass.png" width="18px"> Password:</td>
+          <td><input type="password" name="password"  required></td>
+
+ </tr>
+
+    <tr>
+         <td><img src ="../images/icdate.png" width="18px"> Date of Birth:</td>
+
+         <td>
+         <select name="Date">
+         <option value="date">Date</option>
+         <option value="one">1</option>
+         <option value="two">2</option>
+         <option value="three">3</option>
+         <option value="four">4</option>
+         </select>
+
+         <select name="Month">
+         <option value="month">Month</option>
+         <option value="january">Jan</option>
+         <option value="february">Feb</option>
+         <option value="march">Mar</option>
+         <option value="april">Apr</option>
+         </select>
+
+
+         <select name="Year">
+         <option value="year">Year</option>
+         <option value="1990">1990</option>
+         <option value="1995">1995</option>
+         <option value="2000">2000</option>
+         <option value="2005">2005</option>
+         </select>
+         </td>
+
+
+ </tr>
+
+
+    <tr>
+
+         <td><img src ="../images/icgender.png" width="18px"> Gender:</td>
+         <td><input type="radio" id="male" name="gender" value="Male"> Male
+         <input type="radio" id="female" name="gender" value="Female"> Female
+        
+        </td>
+       
+ </tr>
+
+
+    <tr>
+
+         <td><img src ="../images/icfile.png" width="18px"> Business lisence:</td>
+         <td> <input type="file" id="myfile" name="myfile" required></td>
+
+
+ </tr>
+
+    <tr>
+
+         <td><img src ="../images/ictin.png" width="18px"> TIN certificate no:</td>
+         <td> <input type="number" name="tinnumber" required></td>
+
+     <tr></tr>
+     <tr></tr>
+     <tr></tr>
+     <tr></tr>
+     <tr></tr>
+     <tr></tr>
+     <tr></tr>
+     <tr></tr>
+     <tr></tr>
+     <tr></tr>
+     <tr></tr>
+     <tr></tr>
+     <tr></tr>
+     <tr></tr>
+     <tr></tr>
+     <tr></tr>
+     <tr></tr>            
+
+        
+    <tr>
+         <td colspan="2" align="center">
+
+            <input type="submit" value="Register">
+            <input type="reset" value="Reset">
+
+         </td>
+ </tr>
+</table>
+</form>
+</fieldset>
+</body>
+</html>
+
+
+
+
+
+
+
+
+         
+
+        
+
+
+
+    
+
+
+
+
+
+
+
+</body>
+</html>
